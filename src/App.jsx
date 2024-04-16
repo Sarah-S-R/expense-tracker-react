ae82a1c82dfb30ef0d0faa6a9e950d84a5bb8352
Sarah <143218168+Sarah-S-R@users.noreply.github.com>
2024-04-16 11:21:54 -0230

    expense tracker set up

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import './App.css';
+
+
+function App() {
+  return (
+      <>
+        <Header/>
+        <div className = "container">
+          <Balance/>
+          <IncomeExpenses/>
+          <TransactionList/>
+          </div>
+      </>
+  );
+}
+
+export default App
